44452a3794c38d8de6cb7eb8bfdf944bacfe5dfb
Tretyakov <Imtr3tyakov@gmail.com>
2023-03-29 18:07:59 +0300

    Added reset password page

--- src/pages/reset_password.jsx
+++ src/pages/reset_password.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Alert, Box, Button, Collapse,  TextField} from "@mui/material";
+import {Alert, Box, Button, Collapse, IconButton, InputAdornment, TextField} from "@mui/material";
 import {useFormik} from "formik";
 import * as Yup from "yup";
 import {API} from "@/utils/API";
@@ -7,21 +7,27 @@ import Router from "next/router";
 import {useEnqueueSnackbar} from "@/components/hooks/useSnackbar";
 import ContainerLayouts from "@/components/layouts/ContainerLayouts";
 import LoadingProgress from "@/components/blocks/LoadingProgress";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import AuthHeader from "@/components/blocks/AuthHeader";
 
-const ForgotPassword = () => {
+const ResetPassword = () => {
     const [isLoading, setIsLoading] = React.useState(false)
+    const [isVisiblePassword, setIsVisiblePassword] = React.useState(false)
     const {openSnackbar} = useEnqueueSnackbar()
     const formik = useFormik({
         initialValues: {
-            email: '',
+            password: '',
+            confirmPassword: '',
         },
         validationSchema: Yup.object({
-            email: Yup
+            password: Yup
                 .string()
-                .email('Не является почтой')
                 .required('Данное поле обязательно'),
-
+            confirmPassword: Yup
+                .string()
+                .oneOf([Yup.ref('password'), null], 'Пароли не равны')
+                .required('Данное поле обязательно'),
         }),
         onSubmit: () => {
             (async () => {
@@ -56,19 +62,47 @@ const ForgotPassword = () => {
             <ContainerLayouts maxWidth='600px' justifyContent='center' alignItems='center'>
                 <Box width='100%' display='flex' flexDirection='column' gap='10px'>
                     <AuthHeader title='Восстановление пароля' withButton/>
+
                     <TextField
                         fullWidth
-                        label="Почта"
-                        name="email"
+                        label="Пароль"
+                        name="password"
                         onBlur={formik.handleBlur}
-                        error={Boolean(formik.errors.email)}
+                        error={Boolean(formik.errors.password)}
                         onChange={formik.handleChange}
-                        type='text'
-                        value={formik.values.email}
+                        type={isVisiblePassword ? 'text' : 'password'}
+                        value={formik.values.password}
+                        InputProps={{
+                            endAdornment: <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => setIsVisiblePassword(!isVisiblePassword)}
+                                    edge="end"
+                                >
+                                    {isVisiblePassword ? <Visibility/> : <VisibilityOff/>}
+                                </IconButton>
+                            </InputAdornment>
+                        }}
+
                         variant="outlined"
                     />
-                    <Collapse in={Boolean(formik.errors.email)} unmountOnExit>
-                        <Alert severity="error">{formik.errors.email}</Alert>
+                    <Collapse in={Boolean(formik.errors.password)} unmountOnExit>
+                        <Alert severity="error">{formik.errors.password}</Alert>
+                    </Collapse>
+                    <TextField
+                        fullWidth
+                        label="Повторите пароль"
+                        name="confirmPassword"
+                        onBlur={formik.handleBlur}
+                        error={Boolean(formik.errors.confirmPassword)}
+                        onChange={formik.handleChange}
+                        type={isVisiblePassword ? 'text' : 'password'}
+
+                        value={formik.values.confirmPassword}
+                        variant="outlined"
+                    />
+                    <Collapse in={Boolean(formik.errors.confirmPassword)} unmountOnExit>
+                        <Alert severity="error">{formik.errors.confirmPassword}</Alert>
                     </Collapse>
                     <Button disabled={isLoading} type='submit' variant='contained'>
                         <LoadingProgress isLoading={isLoading} title='Восстановить пароль'/>
@@ -80,4 +114,4 @@ const ForgotPassword = () => {
     );
 };
 
-export default ForgotPassword;
+export default ResetPassword;
